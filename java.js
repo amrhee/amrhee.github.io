2bd7c142d8ff4631caa80f8b1aafc2b15dda659a
amrhee <amrhesham1912@gmail.com>
2025-04-13 19:36:42 +0200

    Update java.js

--- java.js
+++ java.js
@@ -2,7 +2,7 @@ cash = window.prompt("😏تدفع كام");
 if (cash >= 100){
   document.write("<h1 class='statement' id='statement-id'>👌أنت شخص مش منوفي</h1>");
 }else{
-  document.write("<h1 class='statement' id='statement-id'>😒أنت شخص منوفي</h1>");
+  document.write("<h1 class='statementm' id='statement-id'>😒أنت شخص منوفي</h1>");
 }
 // if (cash >= 100){
 //   document.getElementById("statement-id").innerHTML="👌أنت شخص مش منوفس";
