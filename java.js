3762a9db4448b1959e319053647ee5a5fb915da2
amrhee <amrhesham1912@gmail.com>
2025-04-13 19:23:48 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 cash = window.prompt("كا");
 if (cash >= 100){
-  document.write("mmjjjjjjjj")
+  document.write("<h1 class="statement" id="statement-id">👌أنت شخص مش منوفس</h1>");
 }else{
-  document.write("mm");
+  document.write("<h1 class="statement" id="statement-id">👌أنت شخص منوفس</h1>")
 }
 // if (cash >= 100){
 //   document.getElementById("statement-id").innerHTML="👌أنت شخص مش منوفس";
