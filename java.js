ebe0bd7481c8a7a9a6b5d0a1e71cd72a615a671d
amrhee <amrhesham1912@gmail.com>
2025-04-15 22:54:28 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,41 @@
+let x,o;
+let win = false;
+noOfTimes = 1;
+function tlx(){
+    document.getElementsByClassName("tl").innerHTML="x";
+    document.getElementById("myid").innerHTML="id x"
+}
+function tmx(){
+    document.getElementsByClassName("tm").innerHTML="x";
+}
+function funn(idd){
+    while (!win) {
+        if (noOfTimes%2 == 0) {
+            document.getElementById(idd).innerHTML="O";
+            console.log("O");
+            document.getElementById("mass").innerHTML="دلوقتي دور&nbsp<b>X</b>";
+        }else{
+            document.getElementById(idd).innerHTML="X";
+            console.log("X");
+            document.getElementById("mass").innerHTML="دلوقتي دور&nbsp<b>O</b>";
+        }
+        
+        
+        
+        if (win) {
+            document.write("x win");
+        }
+        noOfTimes++;
+        break;
+    }
+}
+function reStart() {
+    document.getElementById().innerHTML="&nbsp&nbsp";
+    document.getElementById().innerHTML="&nbsp&nbsp";
+    document.getElementById().innerHTML="&nbsp&nbsp";
+    
+}
+function funnn(idd){
+    // document.getElementsByClassName(clas).innerHTML="x";
+    document.getElementById(idd).innerHTML="X";
+}
