35ac91a7d93a66ae55b9636e4db180613c797d7a
amrhee <amrhesham1912@gmail.com>
2025-04-16 18:43:26 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,6 +1,14 @@
-let x,o;
+let xWin,oWin;
 let win = false;
 noOfTimes = 1;
+let player = "x";
+let owin = "O فاز!", xwin = "X فاز!";
+let xtl = false, xtm = false , xtr = false;
+let xml = false, xmm = false , xmr = false;
+let xbl = false, xbm = false , xbr = false;
+let otl = false, otm = false , otr = false;
+let oml = false, omm = false , omr = false;
+let obl = false, obm = false , obr = false;
 function tlx(){
     document.getElementsByClassName("tl").innerHTML="x";
     document.getElementById("myid").innerHTML="id x"
@@ -13,17 +21,139 @@ function funn(idd){
         if (noOfTimes%2 == 0) {
             document.getElementById(idd).innerHTML="O";
             console.log("O");
+            switch (idd) {
+            case "tl":
+                otl = true;
+                break;
+            case "tm":
+                otm = true;
+                break;
+            case "tr":
+                otr = true;
+                break;
+            case "ml":
+                oml = true;
+                break;
+            case "mm":
+                omm = true;
+                break;
+            case "mr":
+                omr = true;
+                break;
+            case "bl":
+                obl = true;
+                break;
+            case "bm":
+                obm = true;
+                break;
+            case "br":
+                obr = true;
+                break;
+        }
             document.getElementById("mass").innerHTML="دلوقتي دور&nbsp<b>X</b>";
         }else{
             document.getElementById(idd).innerHTML="X";
             console.log("X");
+            switch (idd) {
+            case "tl":
+                xtl = true;
+                break;
+            case "tm":
+                xtm = true;
+                break;
+            case "tr":
+                xtr = true;
+                break;
+            case "ml":
+                xml = true;
+                break;
+            case "mm":
+                xmm = true;
+                break;
+            case "mr":
+                xmr = true;
+                break;
+            case "bl":
+                xbl = true;
+                break;
+            case "bm":
+                xbm = true;
+                break;
+            case "br":
+                xbr = true;
+                break;
+        }
             document.getElementById("mass").innerHTML="دلوقتي دور&nbsp<b>O</b>";
         }
-        
-        
-        
-        if (win) {
-            document.write("x win");
+        ///////////////////////////////
+        if (xtl == true && xtm == true && xtr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=xwin;
+        }
+        if (xml == true && xmm == true && xmr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=xwin;
+        }
+        if (xbl == true && xbm == true && xbr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=xwin;
+        }
+        ////////////////////////////////
+        if (xtl == true && xml == true && xbl == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=xwin;
+        }
+        if (xtm == true && xmm == true && xbm == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=xwin;
+        }
+        if (xtr == true && xmr == true && xbr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=xwin;
+        }
+        /////////////////////////////////
+        if (xtl == true && xmm == true && xbr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=xwin;
+        }
+        if (xbl == true && xmm == true && xtr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=xwin;
+        }
+        /////////////      O     //////////////////
+        if (xtl == true && otm == true && otr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=owin;
+        }
+        if (oml == true && omm == true && omr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=owin;
+        }
+        if (obl == true && obm == true && obr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=owin;
+        }
+        ////////////////////////////////
+        if (otl == true && oml == true && obl == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=owin;
+        }
+        if (otm == true && omm == true && obm == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=owin;
+        }
+        if (otr == true && omr == true && obr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=owin;
+        }
+        /////////////////////////////////
+        if (otl == true && omm == true && obr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=owin;
+        }
+        if (obl == true && omm == true && otr == true) {
+            win == true;
+            document.getElementById("mass").innerHTML=owin;
         }
         noOfTimes++;
         break;
